5fa77972117ce32e73a08aafbf99f23a99a89606
David Jared <davidjared123@gmail.com>
2021-06-03 15:25:51 -0400

    seciones

---
+++ src/public/js/navbar.js
@@ -0,0 +1,36 @@
+
+
+const sections = document.querySelectorAll('section');
+const sectionsArr = Array.from(sections);
+
+const navItems = document.querySelectorAll('nav li a');
+function activateNavByIndex(index) {
+  if (sections[index].classList.contains('active'))
+     return;
+
+  const currentActive = document.querySelectorAll('nav .active');
+  for (let i = currentActive.length - 1; i >= 0; i--) {
+    currentActive[i].classList.remove('active');
+  }
+  navItems[index].classList.add('active');
+}
+
+const intersectionCallback = (entries, observer) => {
+  if (entries[0].intesectionRatio <= 0)
+      return;
+  
+  console.log(entries);
+  if (entries[0].intersectionRatio > 0.75) {    activateNavByIndex(sectionsArr.indexOf(entries[0].target))
+  }
+};
+
+const intersectionOptions = {
+  threshold: [0, 0.5, 1],
+  rootMargin: '0px 0px 0px 0px'
+};
+
+const intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
+
+for (let i = 0; i < sections.length; i++) {
+  intersectionObserver.observe(sections[i]);  
+}
