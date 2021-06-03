725790f7f1355dcf5c5daf2ead00c8e0d9800ccb
David Jared <davidjared123@gmail.com>
2021-06-03 18:18:44 -0400

    navbar - intersection -Listo

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,24 +1,26 @@
 const sections = document.querySelectorAll("section")
-const navLink = document.querySelectorAll('.nav-list li')
+const navLink = document.querySelectorAll('.nav-list li a')
 
 const options = {
 	threshold: "0.5" // 0.6 == 60% => section
 }
 
+
+
 const observer = new IntersectionObserver(entries =>{
 	entries.forEach(e =>{
-		navLink.forEach((link)=>{
-			// link.classList.remove('active')
-
-			if(e.target.id === link.dataset.nav){
-				link.classList.add('active')
-			}
-		})
-
-		console.log(navLink)
+		
 		if(e.isIntersecting){
-			console.log(e.target.id)
-			navLink.map((item)=> console.log(item))
+			// console.log(e.target.id)
+			navLink.forEach(function (key) {
+				if(key.dataset.nav === e.target.id){
+					key.classList.add('active')
+				} else if(e.target.id === home){
+					console.log('object')
+				} else{
+					key.classList.remove('active')
+				}
+			});
 		}
 		
 	})
